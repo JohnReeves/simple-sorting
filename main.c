df851eaebb986296ff35e1bdf8fd2cd415c1fa78
John Reeves <johnreeves.mathsandcomputing@gmail.com>
2019-12-15 10:20:07 +0000

    removed dead code, simplified translation code

--- main.c
+++ main.c
@@ -9,77 +9,56 @@ int main(void) {
 #include <stdlib.h>
 #include <string.h>
 
-#define SWAP(a,b)   { int t; t=a; a=b; b=t; }
-#define INDEX 8
-
-void bubble_srt(int a[], int n);
-
-
 char cyphertext[]="WKH HDVLHVW PHWKRG RI HQFLSKHULQJ D WHAW PHVVDJH LV WR UHSODFH HDFK FKDUDFWHU EB DQRWKHU XVLQJ D ILAHG UXOH, VR IRU HADPSOH HYHUB OHWWHU D PDB EH UHSODFHG EB G, DQG HYHUB OHWWHU E EB WKH OHWWHU H DQG VR RQ.";
 char alphabet[]="abcdefghijklmnopqrstuvwxyz";
 
 int main(void) 
 {
    int i;
-   int array[INDEX] = {12, 9, 4, 99, 120, 1, 3, 10};
-   char * john = "John\0";
+   char * name = "Your Name\0";
 
-   printf("Hello %s, ", john);
-   // printf("forwards: ");
-   for (i=0; i<strlen(john);i++) {
-      printf("%c", john[i]);
+   printf("Hello %s, ", name);
+
+   // removing spaces, printing forward
+   for (i = 0; i < strlen(name); i++) {
+      if (name[i] != ' ') printf("%c", name[i]);
    }
     printf(" or ");
 
-    // printf("backwards: ");
-    for (i=strlen(john); i>=0;i--) {
-      printf("%c", john[i]);
+    // keeping spaces, printing forwards
+    for (i = strlen(name); i >= 0; i--) {
+      printf("%c", name[i]);
     }
      printf("\n");
 
-   printf("\nSome numbers: ");
-   for(i = 0; i < INDEX; i++)
-      printf("%d ", array[i]);
-   printf("\n");
-     
-   bubble_srt(array, INDEX);
-   
-   printf("Sorted:        ");
-   for(i = 0; i < INDEX; i++)
-      printf("%d ", array[i]);
-   printf("\n");   
-
-   // system("pause"); doesn't do anything here
-
-   
-  printf("\nSooo, the cypher challenge text is :\n");
+  printf("\nThe cypher challenge text is :\n");
   for (i=0; i<strlen(cyphertext); i++){
     if (i%30 == 0) printf("\n");
+    printf("%c", cyphertext[i]);
+  }
+
+  printf("\n\nAaand, translated the text is :\n");
+  for (i=0; i<strlen(cyphertext); i++){
+    // formatting
+    if (i%30 == 0) printf("\n");
+
+    // translated letters
+    if (cyphertext[i] == 'W') printf("t"); else
+    if (cyphertext[i] == 'K') printf("h"); else
+    if (cyphertext[i] == 'H') printf("e"); else
+    // trying these ones
+    if (cyphertext[i] == 'D') printf("a"); else
+    if (cyphertext[i] == 'V') printf("s"); else
+
+    // display the letter we don't know yet
     printf("%c", cyphertext[i]);
 
   }
 
-  printf("\nAaand, translated the text is :\n");
-  for (i=0; i<strlen(cyphertext); i++){
-    if (i%30 == 0) printf("\n");
-    if (cyphertext[i] == 'W') printf("t");
-    if (cyphertext[i] == 'K') printf("h");
-    if (cyphertext[i] == 'H') printf("e");
-    else {
-       printf("%c", cyphertext[i]);
-    }
-
-  }
-   printf("\n");
-   return 0;
+  printf("\n\n");
+  printf("[We think we know: \"WKH\" is \"the\"]\n");
+  printf("[We are trying . : \"D\" as \"a\" and \"V\" as \"s\"]\n");
+    
+  return 0;
 } 
 
-void bubble_srt( int a[], int n ) {   
-    int i, j;     
-    for(i = 0; i < n; i++){              
-        for(j = 1; j < (n-i); j++){              
-           if(a[j-1] > a[j]) 
-              SWAP(a[j-1],a[j]);   
-        }
-    }
-}
